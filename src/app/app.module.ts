acc22c98024f3f9f1837335d57fff77c6bc233d6
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-11-12 18:28:29 -0500

    Initial configuration done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }    from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent }    from './app.component';
+import { HomeComponent }   from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SpotifyService }  from './services/spotify.service';
+
+import {APP_ROUTING}       from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
